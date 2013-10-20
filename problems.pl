bae11d3053c5b7b6b85b639d282b2c8864270245
Richard <barajasr89@gmail.com>
2013-10-20 15:22:36 -0700

    creep_list -> creap_list changes.

--- problems.pl
+++ problems.pl
@@ -393,13 +393,13 @@ rnd_permu([X|Xs], [Y|Rest]):-
 combination(0, _, []).
 combination(N, List, [X|Rest]) :-
     N > 0,
-    creep_list(X, List, Xs),
+    creap_list(X, List, Xs),
     NewN is N-1,
     combination(NewN, Xs, Rest).
     
-creep_list(X, [X|Xs], Xs).
-creep_list(X, [_|Ys], Rest):-
-    creep_list(X, Ys, Rest).
+creap_list(X, [X|Xs], Xs).
+creap_list(X, [_|Ys], Rest):-
+    creap_list(X, Ys, Rest).
 
 %========================================================================
 % P27 (**) Group the elements of a set into disjoint subsets.
@@ -435,7 +435,7 @@ group3(List, Group1, Group2, Group3):-
 
 selectN(0, _, []).
 selectN(N, List, [X|Xs]):-
-    creep_list(X, List, Rest),
+    creap_list(X, List, Rest),
     NewN is N - 1,
     selectN(NewN, Rest, Xs).
 
@@ -446,3 +446,25 @@ group(List,[N|Ns],[Group|Groups]) :-
     subtract(List, Group, Rest),
     group(Rest, Ns, Groups).
 
+% P28 (**) Sorting a list of lists according to length of sublists
+% a) We suppose that a list (InList) contains elements that are lists
+%    themselves. The objective is to sort the elements of InList according
+%    to their length. E.g. short lists first, longer lists later, or vice
+%    versa.
+%
+%   ?- lsort([[a,b,c],[d,e],[f,g,h],[d,e],[i,j,k,l],[m,n],[o]],L).
+%   L = [[o], [d, e], [d, e], [m, n], [a, b, c], [f, g, h], [i, j, k, l]]
+%
+% b) Again, we suppose that a list (InList) contains elements that are lists
+%    themselves. But this time the objective is to sort the elements of 
+%    InList according to their length frequency; i.e. in the default, where
+%    sorting is done ascendingly, lists with rare lengths are placed first,
+%    others with a more frequent length come later.
+%
+%   ?- lfsort([[a,b,c],[d,e],[f,g,h],[d,e],[i,j,k,l],[m,n],[o]],L).
+%   L = [[i, j, k, l], [o], [a, b, c], [f, g, h], [d, e], [d, e], [m, n]]
+%
+% Note that in the above example, the first two lists in the result L have
+% length 4 and 1, both lengths appear just once. The third and forth list
+% have length 3; there are two list of this length. And finally, the last
+% three lists have length 2. This is the most frequent length.
