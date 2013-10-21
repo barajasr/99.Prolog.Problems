66a6eef029f5032385cb778e2b4ebaadf6286f37
Richard <barajasr89@gmail.com>
2013-10-21 15:04:58 -0700

    Addition of P3.01-04. P3.05 to be completed soon or later.

--- problems.pl
+++ problems.pl
@@ -1,4 +1,4 @@
-% Working with Prolog lists
+%% Working with Prolog lists
 
 %=========================================================================
 % P1.01 (*) Find the last element of a list.
@@ -694,7 +694,7 @@ next_number(N, NewN):-
 % Leverage backtracking to test set of primes
 goldbach(N, Pair):-
     list_of_primes(2, N, Primes),
-    !, goldbach(N, Primes, Pair).
+    goldbach(N, Primes, Pair).
 
 goldbach(N, Primes, [X,Y]):-
     creap_list(X, Primes, _),
@@ -755,4 +755,176 @@ print(N, X, Y, Limit):-
     write(X), write(' + '),
     write(Y), nl.
 print(_, _, _, _).              % Suppress output when below Limit
-    
+
+%========================================================================
+%========================================================================
+%% Logic and Codes
+
+%========================================================================
+% P3.01 (**) Truth tables for logical expressions.
+% Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 
+% (for logical equivalence) whichsucceed or fail according to the result
+% of their respective operations; e.g. and(A,B) will succeed, if and only
+% if both A and B succeed. Note that A and B can be Prolog goals (not
+% only the constants true and fail).
+% 
+% A logical expression in two variables can then be written in prefix
+% notation, as in the following example:and(or(A,B),nand(A,B)).Now, write
+% a predicate table/3 which prints the truth table of a given logical
+% expression in two variables.
+%
+%   ?- table(A,B,and(A,or(A,B))).
+%   true true true
+%   true fail true
+%   fail true fail
+%   fail fail fail
+and(X, Y):- X, Y.
+
+or(X, _):- X.
+or(_, X):- X.
+
+equ(X, Y):- or(and(X, Y), and(not(X), not(Y))).
+
+nand(X, Y):- not(and(X, Y)).
+
+nor(X, Y):- not(or(X, Y)).
+
+xor(X, Y):- not(equl(X, Y)).
+
+impl(X, Y):- or(not(X), Y).
+
+bind(true).
+bind(fail).
+
+table(A,B,Expr):-
+    bind(A),
+    bind(B),
+    do(A,B,Expr),
+    fail.
+
+do(A,B,_):-
+    write(A),
+    write('  '),
+    write(B),
+    write('  '),
+    fail.
+do(_,_,Expr):-
+    Expr,
+    !, write(true), nl.
+do(_,_,_):-
+    write(fail), nl.
+
+%========================================================================
+% P3.02 (*) Truth tables for logical expressions (2).
+% Continue problem P3.01 by defining and/2, or/2, etc as being operators.
+% This allows to write the logical expression in the more natural way, as
+% in the example: A and (A or not B). Define operator precedence as usual;
+% i.e. as in Java.
+%
+% ?- table(A,B, A and (A or not B)).
+% true  true  true
+% true  fail  true
+% fail  true  fail
+% fail  fail  fail
+:- op(600, fy,not).
+:- op(700, yfx, and).
+:- op(700, yfx, nand).
+:- op(800, yfx, or).
+:- op(800, yfx, nor).
+:- op(900, yfx, impl).
+:- op(900, yfx, equ).
+:- op(900, yfx, xor).
+
+%========================================================================
+% P3.03 (*) Truth tables for logical expressions (3).
+% Generalize problem P3.02 in such a way that the logical expression may
+% contain any number of logical variables. Define table/2 in a way that 
+% table(List, Expr) prints the truth table for Expr, which contains the
+% logical variables enumerated in List.
+%
+%   ?- table([A,B,C], A and (B or C) equ A and B or A and C).
+%   true true true true
+%   true true fail true
+%   true fail true true
+%   true fail fail true
+%   fail true true true
+%   fail true fail true
+%   fail fail true true
+%   fail fail fail true
+table(VarList, Expr):-
+    bindList(VarList),
+    do(VarList,Expr),
+    fail.
+
+bindList([]).
+bindList([X|Xs]):-
+    bind(X),
+    bindList(Xs).
+
+do(VarList,Expr):-
+    writeVarList(VarList),
+    writeExpr(Expr), nl.
+
+writeVarList([]).
+writeVarList([X|Xs]):-
+    write(X),
+    write('  '),
+    writeVarList(Xs).
+
+writeExpr(Expr):-
+    Expr,
+    !, write(true).
+writeExpr(_):-
+    write(fail).
+
+%========================================================================
+% P3.04 (**) Gray code.
+% An n-bit Gray code is a sequence of n-bit strings constructed according
+% to certain rules. For example,
+%   n = 1: C(1) = ['0','1'].
+%   n = 2: C(2) = ['00','01','11','10'].
+%   n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
+%
+% Find out the construction rules and write a predicate with the following
+% specification:
+%   % gray(N,C) :- C is the N-bit Gray code
+%
+% Can you apply the method of "result caching" in order to make the
+% predicate more efficient, when it is to be used repeatedly?
+%   Make predicate dynamic, and include asserta line for current args.
+%   Solutions of m <= N should now be in listing(gray_code/2),
+%   for future use.
+
+%:- dynamic gray_code/2.
+gray_code(1, ['0', '1']).
+gray_code(N, Code):-
+    N > 1,
+    NewN is N - 1,
+    gray_code(NewN, SubCode),
+    reverse(SubCode, ReversedSubCode),
+    prepend('0', SubCode, FirstHalf),
+    prepend('1', ReversedSubCode, SecondHalf),
+    append(FirstHalf, SecondHalf, Code).
+%    asserta((gray_code(N, Code):- !)).
+
+prepend(_, [], []).
+prepend(X, [Code|Codes], [NewCode|NewCodes]):-
+    atom_concat(X, Code, NewCode),
+    prepend(X, Codes, NewCodes).
+
+%========================================================================
+% P3.05 (***) Huffman code.
+% First of all, consult a good book on discrete mathematics or algorithms
+% for a detailed description of Huffman codes!
+%
+% We suppose a set of symbols with their frequencies, given as a list of
+% fr(S,F) terms. Example: 
+% [fr(a,45),fr(b,13),fr(c,12),fr(d,16),fr(e,9),fr(f,5)]. Our objective is
+% to construct a list hc(S,C) terms, where C is the Huffman code word for
+% the symbol S. In our example, the result could be Hs = [hc(a,'0'),
+% hc(b,'101'), hc(c,'100'), hc(d,'111'), hc(e,'1101'), hc(f,'1100')]
+% [hc(a,'01'),...etc.]. The task shall beperformed by the predicate
+% huffman/2 defined as follows:
+%
+%   % huffman(Fs,Hs) :- Hs is the Huffman code table for the frequency
+% table Fs
